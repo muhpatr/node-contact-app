3f08d7f19085474762e958d09a9025e0e5127fa6
Muhammad Patria <muhpatr@gmail.com>
2016-03-19 00:05:48 +0700

    Add dummy data on controller.js
    Add dummy data on controller.js, and then call it on index.html using
    ng-repeat to the <tr>

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,24 @@
-function AppCtrl() {
-	console.log('Hello world from controller')
+function AppCtrl($scope) {
+	console.log('Hello world from controller');
+	
+	person1 = {
+		name: 'Patr',
+		email: 'muh@patr.com',
+		number: '(021) 333-3333'
+	};
+	
+	person2 = {
+		name: 'John',
+		email: 'terry@john.com',
+		number: '(021) 262-2626'
+	};
+	
+	person3 = {
+		name: 'Costa',
+		email: 'diego@costa.com',
+		number: '(021) 191-1919'
+	};
+	
+	var contactList = [person1, person2, person3];
+	$scope.contactList = contactList;
 }
