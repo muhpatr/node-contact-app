672f016ab56c9406836007d4ed7f0c74d545ffe7
Muhammad Patria <muhpatr@gmail.com>
2016-03-19 01:59:18 +0700

    Automatically refresh the contact list
    Update controller.js by putting the $http.get on new function and call
    this function on the next line and on the $http.post

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,15 +1,21 @@
 function AppCtrl($scope, $http) {
 	console.log('Hello world from controller');
 	
-	$http.get('/contactList').success(function(res) {
-		console.log('I got the data I requested');
-		$scope.contactList = res;
-	});
+	var refresh = function() {
+		$http.get('/contactList').success(function(res) {
+			console.log('I got the data I requested');
+			$scope.contactList = res;
+			$scope.contact = "";
+		});
+	};
+	
+	refresh();
 	
 	$scope.addContact = function() {
 		console.log($scope.contact);
 		$http.post('/contactList', $scope.contact).success(function(res) {
 			console.log(res);
+			refresh();
 		});
 	};
 }
