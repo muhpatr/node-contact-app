8a08739974a5e7fc42fb45b57bc401aad1e1e219
Muhammad Patria <muhpatr@gmail.com>
2016-03-19 00:14:49 +0700

    Learning about HTTP GET request
    Move dummy data to server.js and get this data on controller.js by using
    $http.get command

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,24 +1,8 @@
-function AppCtrl($scope) {
+function AppCtrl($scope, $http) {
 	console.log('Hello world from controller');
 	
-	person1 = {
-		name: 'Patr',
-		email: 'muh@patr.com',
-		number: '(021) 333-3333'
-	};
-	
-	person2 = {
-		name: 'John',
-		email: 'terry@john.com',
-		number: '(021) 262-2626'
-	};
-	
-	person3 = {
-		name: 'Costa',
-		email: 'diego@costa.com',
-		number: '(021) 191-1919'
-	};
-	
-	var contactList = [person1, person2, person3];
-	$scope.contactList = contactList;
+	$http.get('/contactList').success(function(res) {
+		console.log('I got the data I requested');
+		$scope.contactList = res;
+	});
 }
--- server.js
+++ server.js
@@ -3,5 +3,30 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req, res) {
+	console.log('I received a GET request')
+	
+	person1 = {
+		name: 'Patr',
+		email: 'muh@patr.com',
+		number: '(021) 333-3333'
+	};
+	
+	person2 = {
+		name: 'John',
+		email: 'terry@john.com',
+		number: '(021) 262-2626'
+	};
+	
+	person3 = {
+		name: 'Costa',
+		email: 'diego@costa.com',
+		number: '(021) 191-1919'
+	};
+	
+	var contactList = [person1, person2, person3];
+	res.json(contactList);
+});
+
 app.listen(3000);
 console.log('Server running on port 3000');
