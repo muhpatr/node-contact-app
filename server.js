5ea883f909fac05e4078b8bb2a30bc0dd56161d7
Muhammad Patria <muhpatr@gmail.com>
2016-03-19 01:30:19 +0700

    Update server.js to use mongoDB
    Install mongojs module using npm:
    >> npm install mongojs
    Create database in mongoDB using mongo:
    >> use contactList
    Insert data:
    >> db.contactList.insert([{record-1}, {record-2}, {record-3}])
    
    Call the data on server.js by adding mongojs line

--- server.js
+++ server.js
@@ -1,31 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactList', function(req, res) {
 	console.log('I received a GET request')
 	
-	person1 = {
-		name: 'Patr',
-		email: 'muh@patr.com',
-		number: '(021) 333-3333'
-	};
-	
-	person2 = {
-		name: 'John',
-		email: 'terry@john.com',
-		number: '(021) 262-2626'
-	};
-	
-	person3 = {
-		name: 'Costa',
-		email: 'diego@costa.com',
-		number: '(021) 191-1919'
-	};
-	
-	var contactList = [person1, person2, person3];
-	res.json(contactList);
+	db.contactList.find(function(err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
 });
 
 app.listen(3000);
